2cf313a4f60b9aa1d49c5caf05ed4b78c7d84274
Kapil-Planit <57516970+Kapil-Planit@users.noreply.github.com>
2019-11-08 16:24:05 +1100

    Initial commit
    first commit

---
+++ CookieExample/Cookie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanitTesting.Cookies
+{
+    class Cookie
+    {
+        private string myflavour;
+        private string mytopping;
+        private int myweight;
+
+        public Cookie(string flavourSample)
+        {
+            this.flavour = flavourSample;
+            this.topping = "chocochip";
+            this.weight = 10;
+
+        }
+
+        public string flavour { get { return myflavour; } set { myflavour = value; } }
+        public string topping { get { return mytopping; } set { mytopping = value; } }
+        public int weight { get { return myweight; } set { myweight = value; } }
+    }
+}
---
+++ CookieExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanitTesting.Cookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cookie cookie1 = new Cookie("choclate");
+            Cookie cookie2 = new Cookie("coconut");
+
+            cookie1.topping= "gemschips";
+            cookie1.weight = 20;
+            cookie2.topping = "cashews";
+            cookie2.weight = 30;
+
+            Console.WriteLine("the flavor, topping and weight is ${cookie1.flavour} ,%s ,%d", cookie1.flavour, cookie1.topping, cookie1.weight );
+            Console.WriteLine("the flavor, topping and weight is" + cookie2.flavour + ", " + cookie2.topping + ", " + cookie2.weight);
+
+        }
+    }
+}
